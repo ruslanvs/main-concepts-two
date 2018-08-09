bc3477c0d72c0752e853cfd8ffab63d4e56d9af1
ruslanvs <ruslanvs@yahoo.com>
2018-08-09 17:05:58 +0300

    Composing components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
   render() {
+    if (this.state.liked) {
+      return 'You liked this. qqq';
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <button onClick={() => this.setState({ liked: true }) }>
+        Like
+      </button>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,32 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+class Q extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome name = "Diana" />
+        <Welcome name = "Sofiya" />
+      </div>
+    );
+  }
+}
+
+
+// function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>;
+// }
+  
+// const element = <Welcome name="Sara" />;
+
+ReactDOM.render(<Q />, document.getElementById('root'));
+// ReactDOM.render(Q, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
