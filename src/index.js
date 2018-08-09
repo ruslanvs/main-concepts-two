388dc64109d46828ae56151a2edf87059cc6365d
ruslanvs <ruslanvs@yahoo.com>
2018-08-09 20:05:02 +0300

    Event handler

--- src/index.js
+++ src/index.js
@@ -4,61 +4,94 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
-}
-
-class Hellos extends React.Component {
-  render() {
-    return (
-      <div>
-        <Hello name = "Diana" />
-        <Hello name = "Sofiya" />
-      </div>
-    );
-  }
-}
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    })
+{
+  class Toggle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+  
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      this.setState(prevState => ({
+        isToggleOn: !prevState.isToggleOn
+      }));
+    }
+  
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
+    }
   }
   
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
+  ReactDOM.render(
+    <Toggle />,
+    document.getElementById('root')
+  );
 }
 
+/*
+{ // Nice basics
+  class Hello extends React.Component {
+    render() {
+      return <h1>Hello, {this.props.name}</h1>;
+    }
+  }
+  
+  class Hellos extends React.Component {
+    render() {
+      return (
+        <div>
+          <Hello name = "Diana" />
+          <Hello name = "Sofiya" />
+        </div>
+      );
+    }
+  }
+  ReactDOM.render(<Hellos />, document.getElementById('root'));
+}
 
-ReactDOM.render(<Clock />, document.getElementById('root'));
-
-
-// ReactDOM.render(<Hellos />, document.getElementById('root'));
+{// Encapsulated clock
+  class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      })
+    }
+    
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+  ReactDOM.render(<Clock />, document.getElementById('root'));
+}
 
 // ReactDOM.render(<App />, document.getElementById('root'));
+*/
 registerServiceWorker();
